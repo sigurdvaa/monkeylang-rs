cb0aa5766df60422217a3ed81422c75610129629
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-12-18 20:54:51 +0100

    update repl

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod parser;
 mod repl;
 use std::io::{stdin, stdout};
 
+const VERSION: &str = "0.1.0";
+
 fn main() {
-    println!("Monkeylang");
+    println!("Monkeylang {VERSION}.\npress ctrl+c to exit.");
     repl::start_repl(stdin().lock(), stdout());
 }
--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,8 +1,35 @@
 use crate::lexer::Lexer;
-use crate::parser::Parser;
+use crate::parser::{Parser, ParserError};
 use std::io::{BufRead, Write};
 
 const PROMPT: &str = ">> ";
+const MONKEY_FACE: &str = concat!(
+    "            __,__\n",
+    "   .--.  .-\"     \"-.  .--.\n",
+    "  / .. \\/  .-. .-.  \\/ .. \\\n",
+    " | |  '|  /   Y   \\  |'  | |\n",
+    " | \\   \\  \\ 0 | 0 /  /   / |\n",
+    "  \\ '- ,\\.-\"\"\"\"\"\"\"-./, -' /\n",
+    "   ''-' /_   ^ ^   _\\ '-''\n",
+    "       |  \\._   _./  |\n",
+    "       \\   \\ '~' /   /\n",
+    "        '._ '-=-' _.'\n",
+    "           '-----'\n",
+);
+
+fn print_parser_errors<O>(output: &mut O, errors: &[ParserError])
+where
+    O: Write,
+{
+    writeln!(
+        output,
+        "{MONKEY_FACE}Woops! We ran into some monkey business here!\n parser errors:"
+    )
+    .expect("writing to output buffer failed");
+    for err in errors {
+        writeln!(output, "\t{err}").expect("writing to output buffer failed")
+    }
+}
 
 pub fn start_repl<I, O>(mut input: I, mut output: O)
 where
@@ -13,15 +40,19 @@ where
         let mut buf = String::new();
         output
             .write_all(PROMPT.as_bytes())
-            .expect("failed to write prompt");
-        // output.flush().expect("failed to flush prompt");
+            .expect("failed to write prompt to output buffer");
+        output
+            .flush()
+            .expect("failed to flush prompt to output buffer");
         input.read_line(&mut buf).expect("invalid input");
+
         let lexer = Lexer::new(None, buf.chars().peekable());
         let mut parser = Parser::new(lexer);
         let program = parser.parse_program();
 
-        for err in parser.errors {
-            writeln!(&mut output, "error: {err}").expect("writing to output buffer failed")
+        if !parser.errors.is_empty() {
+            print_parser_errors(&mut output, &parser.errors);
+            continue;
         }
 
         writeln!(&mut output, "{program}").expect("writing to output buffer failed")
